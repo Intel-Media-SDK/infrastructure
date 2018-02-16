103d545c4f2cb1b7ba922e5a1fb640b0182d0422
Dydychkin <alexander.dydychkin@intel.com>
2018-02-16 16:19:50 +0300

    Fixes and add more secrets

--- bb/master/config.py
+++ bb/master/config.py
@@ -26,6 +26,7 @@ import msdk_secrets
 
 class Mode(Enum):
     PRODUCTION_MODE = "production_mode"
+    PRODUCTION_MODE_EMBEDDED = "production_mode_embedded"
     TEST_MODE = "test_mode"
 
 BUILD = "build"
@@ -56,7 +57,6 @@ WORKER_PASS = msdk_secrets.WORKER_PASS
 DATABASE_PASSWORD = msdk_secrets.DATABASE_PASSWORD
 DATABASE_URL = f"postgresql://buildbot:{DATABASE_PASSWORD}@localhost/buildbot"
 GITHUB_TOKEN = msdk_secrets.GITHUB_TOKEN
-GITHUB_WEBHOOK_SECRET = msdk_secrets.GITHUB_WEBHOOK_SECRET
 GITHUB_OWNER = "Intel-Media-SDK"
 
 CURRENT_MODE = Mode.PRODUCTION_MODE
@@ -68,9 +68,9 @@ if CURRENT_MODE == Mode.PRODUCTION_MODE:
     BUILDBOT_URL = "http://mediasdk.intel.com/buildbot/"
 
 elif CURRENT_MODE == Mode.PRODUCTION_MODE_EMBEDDED:
-    WORKERS = {BUILD: {"b-50-41": {}}}
-    GITHUB_OWNERS_REPO = "Next-GEN"
-    BUILDBOT_URL = "http://10.125.50.41:5000/"
+    WORKERS = msdk_secrets.WORKERS
+    GITHUB_OWNERS_REPO = msdk_secrets.EMBEDDED_REPO
+    BUILDBOT_URL = msdk_secrets.BUILDBOT_URL
 
 elif CURRENT_MODE == Mode.TEST_MODE:
     DATABASE_URL = "sqlite:///state.sqlite" # Only for test mode
