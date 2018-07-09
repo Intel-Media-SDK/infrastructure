fe1bb9f9870c39bf57a506af67a678250ec15a0c
Eugene Ponomarev <eugene.ponomarev@intel.com>
2018-07-09 17:55:59 +0300

    used 'windows_uwp' instead 'cmrt'

--- build_scripts/build_runner.py
+++ build_scripts/build_runner.py
@@ -885,7 +885,7 @@ which is not present in mediasdk_directories.''')
                         help='Type of build')
     parser.add_argument('-p', "--product-type", default='linux',
                         choices=['linux', 'embedded', 'open_source', 'windows',
-                                 'api_latest', 'embedded_private', 'android', 'cmrt'],
+                                 'windows_uwp', 'api_latest', 'embedded_private', 'android'],
                         help='Type of product')
     parser.add_argument('-e', "--build-event", default='commit',
                         choices=['pre_commit', 'commit', 'nightly', 'weekly'],
