87d6f0a0394df2c7ffbf3f42852f0dc03675f0f2
Dmitriy Lobanov <dmitriyx.lobanov@intel.com>
2018-11-21 15:13:44 +0300

    [Build scripts] Added argument for hiding secured command line parts (#157)
    * [Build scripts] Added argument for hiding secured command line parts
    
    * [Build scripts] Added argument for hiding secured command line parts

--- common/helper.py
+++ common/helper.py
@@ -569,17 +569,21 @@ def update_json(check_type, success, output, json_path):
     return True
 
 
-def cmd_exec(cmd, env=None, cwd=None, shell=True, log=None, verbose=True):
+def cmd_exec(cmd, env=None, cwd=None, shell=True, log=None, verbose=True, hide=None):
+
+    out_cmd = subprocess.list2cmdline(cmd) if isinstance(cmd, list) else cmd
+
+    if isinstance(hide, list):
+        for hide_str, show_str in hide:
+            out_cmd = out_cmd.replace(hide_str, show_str)
+
     if log:
         if verbose:
             log_out = log.info
         else:
             log_out = log.debug
 
-        if isinstance(cmd, list):
-            log_out(f'cmd: {subprocess.list2cmdline(cmd)}')
-        else:
-            log_out(f'cmd: {cmd}')
+        log_out(f'cmd: {out_cmd}')
 
         if not cwd:
             cwd = str(pathlib.Path.cwd())
