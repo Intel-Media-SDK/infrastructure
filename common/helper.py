f444532a0b1de4beef5bc12c1279145b3143e699
Eugene Ponomarev <eugene.ponomarev@intel.com>
2020-02-19 14:17:39 +0300

    [Buildbot] Added 2 driver builds with kernel option (#371)
    * [Buildbot] Added 2 driver builds with kernel option
    
    * Update copyright

--- common/helper.py
+++ common/helper.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 Intel Corporation
+# Copyright (c) 2019-2020 Intel Corporation
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -138,6 +138,8 @@ class Product_type(Enum):
     PUBLIC_LINUX_DRIVER = 'public_linux_driver'
     PUBLIC_LINUX_DRIVER_CLANG = 'public_linux_driver_clang_9.0'
     PUBLIC_LINUX_DRIVER_GCC_LATEST = 'public_linux_driver_gcc_9.2'
+    PUBLIC_LINUX_DRIVER_KERNELS_OFF = 'public_linux_driver_kernels_off'
+    PUBLIC_LINUX_DRIVER_NONFREE_KERNELS_OFF = 'public_linux_driver_nonfree_kernels_off'
 
     # Components
     PUBLIC_LINUX_LIBVA = 'public_linux_libva'
